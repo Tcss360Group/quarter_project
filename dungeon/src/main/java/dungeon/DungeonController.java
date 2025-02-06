738cd5b114d3e6c9deb0b2b5df62ec269c99d424
KylerAce <kylerlumpkin1@gmail.com>
2025-02-05 19:43:20 -0800

    creates hello world maven project that compiles

---
+++ dungeon/src/main/java/dungeon/DungeonController.java
@@ -0,0 +1,11 @@
+package dungeon;
+
+/**
+ * Hello world!
+ *
+ */
+public class DungeonController {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
